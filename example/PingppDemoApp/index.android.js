2e850e535ededb6bd202a523dd62e864759f49cd
afon <wengxufeng@gmail.com>
2017-03-22 20:49:35 +0800

    update demoapp

---
+++ example/PingppDemoApp/index.android.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Button,
+    Alert,
+    TextInput,
+    ListView,
+    View,
+    ToastAndroid,
+    ProgressBarAndroid
+} from 'react-native';
+
+var Pingpp = require('pingpp-react-native');
+var NetUtil = require('./netutil.js');
+
+export default class PingppDemoApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("alipay")}>支付宝</Text>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("wx")}>微信支付</Text>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("upacp")}>银联支付</Text>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("qpay")}>QQ钱包</Text>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("bfb_wap")}>百付宝wap</Text>
+        <Text style={styles.payBtn} onPress={() => this.textOnclick("jdpay_wap")}>京东支付wap</Text>
+        <Text style={styles.payBtn} onPress={() => Pingpp.setDebug(true)}>开启调试</Text>
+      </View>
+    );
+  }
+
+  textOnclick(channel) {
+    NetUtil.postJson(
+      "http://218.244.151.190/demo/charge",
+      {amount: 1, channel: channel, order_no: new Date().getTime()},
+      function(object) {
+        Pingpp.createPayment(object, function(result) {
+          //JSON.parse(result);
+          ToastAndroid.show("react-result:" + result, ToastAndroid.SHORT);
+        });
+      }
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  payBtn: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor:"#DEDEDE",
+    padding:5
+  },
+});
+
+AppRegistry.registerComponent('PingppDemoApp', () => PingppDemoApp);
---
+++ example/PingppDemoApp/index.ios.js
@@ -0,0 +1,85 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+var NetUtil = require('./netutil.js');
+var Pingpp = require('pingpp-react-native');
+
+export default class PingppDemoApp extends Component {
+  setDebugModel(enabled) {
+    Pingpp.setDebugModel(enabled);
+  };
+
+  pay(channel) {
+    NetUtil.postJson(
+      "http://218.244.151.190/demo/charge",
+      {amount: 1, channel: channel, order_no: new Date().getTime()},
+      function(object) {
+        Pingpp.createPayment(
+          {
+            "object":object,
+            "scheme":"pingppdemoapp"
+          }, function(res, error) {
+            alert(res);
+            console.log(error);
+            alert(JSON.stringify(error));
+          }
+        );
+      }
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress = {()=>{this.pay("alipay")}}>
+          <Text style={styles.welcome}>支付宝</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{this.pay("wx")}}>
+          <Text style={styles.welcome}>微信</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{this.pay("upacp")}}>
+          <Text style={styles.welcome}>银联</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{this.pay("qpay")}}>
+          <Text style={styles.welcome}>QQ钱包</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{this.setDebugModel(true)}}>
+          <Text style={styles.welcome}>开启调试</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('PingppDemoApp', () => PingppDemoApp);
